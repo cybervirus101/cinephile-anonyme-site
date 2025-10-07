2bf5aa4bc23f7d2ba966f3edf8a58a6eaa556849
cybervirus101 <236508078+cybervirus101@users.noreply.github.com>
2025-10-07 00:32:30 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif',
+      flexDirection: 'column'
+    }}>
+      <h1>🎬 Cinéphile Anonyme</h1>
+      <p>Bienvenue sur le site du serveur Discord !</p>
+    </div>
+  )
+}
